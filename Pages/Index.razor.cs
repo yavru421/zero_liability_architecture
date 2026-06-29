5129b0ef1df563f3886d71b9b23295034b85966c
J.Dondlinger <dondlingergeneralcontracting@gmail.com>
2026-06-29 16:27:00 -0500

    Refactor Swipe Deck for B2B ZLA pitch

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -46,8 +46,8 @@ public partial class Index : ComponentBase, IAsyncDisposable
 
     private readonly string[] rhymes = new[]
     {
-        "There once was a Server named Sneeker-Mc-Snoot, Who rented out tools in exchange for your loot. 'Upload your data, your files, and your text! My servers will process and see what comes next!' But when your data is locked in his cloud, It’s watched by the Sneekers and sold to the crowd.",
-        "He makes you create an account and a key, And lock up your secrets for hackers to see. For databases are targets for sneaks in the night, Who smash through the logins and run in a fright! If there is no server to hold what you make, There is nothing to steal and no passwords to break!",
+        "There once was a Server named Sneeker-Mc-Snoot, Who charged monthly rent just to process your loot! 'Give me a thousand a month!' he would cry, 'Or your app will go down and your business will die!' He scales up your bill when your traffic takes flight, And bleeds your accounts in the dead of the night!",
+        "He makes you write policies, hire a crew, Just to prove that the data he's holding is true! He forces you into compliance and fear, And makes you buy cyber-insurance each year! If there is no server to hold what you make, There is zero compliance and nothing at stake!",
         "So we built you a hammer, a physical tool, Delivered right to you, so simple and cool! There is no backend, no server to call, It runs in your browser, and that is just all. The tool does the work right in front of your face, With zero connections to any outside space!",
         "Don't believe it? Just sever the cord! Turn off your Wi-Fi and pull out the board! Type in your numbers and see what they do, The engine is local and powered by YOU! The processing happens right here on your screen, The most private computer that ever has been!",
         "We never take custody, never take hold, Your data remains yours, it can never be sold. We have zero liability, zero to hide, Your secrets are safe on your own local side! And when you are finished and close up the tab, Your data is wiped with no traces to grab!"
